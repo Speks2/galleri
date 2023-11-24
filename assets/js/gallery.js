750d3641c3c65cd75bdfbef5b4a819e99d085887
Speks2 <142408255+Speks2@users.noreply.github.com>
2023-11-24 16:18:25 +0100

    galleryyy

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,9 @@
 // Din cool code here
+var imageUrls = [
+    'https://unsplash.com/s/photos/elephants',
+    'https://stock.adobe.com/dk/search?k=great%20white%20shark',
+    'https://unsplash.com/s/photos/koala',
+    'https://unsplash.com/s/photos/tarantula',
+    'https://unsplash.com/images/animals/tiger',
+  
+  ];
